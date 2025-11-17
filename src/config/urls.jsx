1f293472010a16e7ad303a2612039642eff2c682
Mohd-Elham <muhammedelham@gmail.com>
2025-11-17 10:20:47 +0530

    v145

--- src/config/urls.jsx
+++ src/config/urls.jsx
@@ -5,7 +5,7 @@ export const IMAGE_URL_LIGHT = 'https://i.ibb.co/BKFt5pPW/logo-dark.png';
 
 export const RAZORPAY_CHECKOUT_JS = 'https://checkout.razorpay.com/v1/checkout.js';
 
-export const API_BASE = 'http://localhost:8030';
+export const API_BASE = 'https://sub.zonion.fun';
 export const API_RAZORPAY_CREATE_ORDER = `${API_BASE}/payment/razorpay/create-order`;
 export const API_RAZORPAY_VERIFY = `${API_BASE}/payment/razorpay/verify`;
 export const API_RAZORPAY_SUCCESS = `${API_BASE}/payment/razorpay/success`;
